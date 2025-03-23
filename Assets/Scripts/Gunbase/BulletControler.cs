c85fb56f3fc2de9190e9a79d31232bb2d72bcda8
ZitHoang <hoangtvhe180179@fpt.edu.vn>
2025-03-23 23:02:58 +0700

    connect scene level_1

--- Assets/Scripts/Gunbase/BulletControler.cs
+++ Assets/Scripts/Gunbase/BulletControler.cs
@@ -10,8 +10,6 @@ public class BulletController : MonoBehaviour
 	private GameObject targetEnemy;
     private BulletObjectPoolManager bulletObjectPoolManager;
 
-    public int damage = 1;
-
     void Start()
     {
         // bulletObjectPoolManager = BulletObjectPoolManager.instance;
