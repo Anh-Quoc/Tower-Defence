1bd7d39d012a5b1f621ccd9752f861d39d21c0e2
hoangnm <nguyenminhhoangtitlal@gmail.com>
2025-03-24 02:14:26 +0700

    Revert "Merge remote-tracking branch 'origin/develop' into hoangnm"
    This reverts commit 1e1bd76d0226a7988527dc13a2348b21ca35c46e, reversing
    changes made to 33e2ac3cf3a2c36f00b3e445ff162ed0e0cf9569.

--- Assets/Scripts/CardUpgrade/GameManager.cs
+++ Assets/Scripts/CardUpgrade/GameManager.cs
@@ -4,66 +4,33 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    private bool gameOver = false;
+
     int currentWave = 0;
-    int gold = 150;
-    [SerializeField] float maxHealth = 1000;
-    float currentHealth = 0;
-    [SerializeField] int waveUnlockUpgrade = 5;
+
     GameState currentState;
 
     public event Action<GameState> OnStateChanged;
 
     private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-
-        currentHealth = maxHealth;
+        Instance = this;
     }
 
     public int GetCurrentWave()
     {
         return currentWave;
     }
-    public void NextWave()
-    {
-        currentWave += 1;
-    }
-    public int GetGold => gold;
-    public bool IsGameOver => gameOver;
-    public void AddGold(int gold) => this.gold += gold;
-    public float GetCurrentHealth => currentHealth;
-    public void TakeDamage(float damage)
-    {
-        if (currentHealth <= 0)
-        {
-            gameOver = true;
-            return;
-        }
-        currentHealth -= damage;
-    }
 
 
+    // Test code to change state
     private void Update()
     {
-        if (gameOver)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            // TODO: Stop the game and show game over screen
-            return;
+            ChangeState(GameState.CardSelection);
+            currentWave++;
+            Debug.Log("Wave: " + currentWave);
         }
-        // if (currentState == GameState.Playing && (currentWave) % waveUnlockUpgrade == 0)
-        // {
-        //     ChangeState(GameState.CardSelection);
-        //     Debug.Log("Wave: " + currentWave);
-        // }
     }
 
     public void ChangeState(GameState newState)
--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,111 +4,40 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] private int poolingSizeEachEnemy = 50;
-    [SerializeField] private List<GameObject> enemyPrefabs;
-    [SerializeField] private List<WaveProperties> waveProperties;
-    [SerializeField] private List<GameObject> waypoints;
-    private Dictionary<GameObject, Queue<GameObject>> enemyPool = new Dictionary<GameObject, Queue<GameObject>>();
-    private List<GameObject> activeObjects = new List<GameObject>();
-    private WaveProperties currentWave;
-    private float spawnWaveDelay;
-    private Coroutine spawnRoutine;
+    [SerializeField] GameObject enemy;
+    [SerializeField] List<GameObject> waypoints;
+    int currentWaypointIndex = 0;
+    float speed = 1f;
+    bool isStopped = false;
 
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        InitializeObjectPool();
+        speed = enemy.GetComponent<EnemyProperties>().getSpeed();
+        enemy.transform.position = waypoints[currentWaypointIndex].transform.position;
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        if(!GameManager.Instance.IsGameOver)
-            HandleWaveSpawning();
+        if (waypoints.Count == 0 || currentWaypointIndex == waypoints.Count || isStopped) return;
+        MoveTowardsWaypoint();
     }
 
-    private void InitializeObjectPool()
+    private void MoveTowardsWaypoint()
     {
-        foreach (var prefab in enemyPrefabs)
+        Transform targetWaypoint = waypoints[currentWaypointIndex].transform;
+        enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, targetWaypoint.position, speed * Time.deltaTime);
+
+        // Rotate the enemy to face the target waypoint
+        Vector3 direction = targetWaypoint.position - enemy.transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; // Calculate angle in degrees
+        enemy.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        if (Vector3.Distance(enemy.transform.position, targetWaypoint.position) < 0.1f)
         {
-            Queue<GameObject> pool = new Queue<GameObject>();
-
-            for (int i = 0; i < poolingSizeEachEnemy; i++)
-            {
-                GameObject enemy = Instantiate(prefab, waypoints[0].transform.position, Quaternion.identity);
-                enemy.GetComponent<EnemyProperties>().Waypoints = waypoints;
-                pool.Enqueue(enemy);
-            }
-
-            enemyPool[prefab] = pool;
+            currentWaypointIndex++;
         }
-    }
-
-    private void HandleWaveSpawning()
-    {
-        if (currentWave == null)
-        {
-            GameManager.Instance.NextWave();
-            waveProperties.Sort((a, b) => a.waveIndex.CompareTo(b.waveIndex));
-
-            currentWave = waveProperties[waveProperties.Count - 1];
-            for (int i = 0; i < waveProperties.Count; i++)
-            {
-                if (waveProperties[i].waveIndex > GameManager.Instance.GetCurrentWave())
-                {
-                    currentWave = waveProperties[i - 1];
-                    break;
-                }
-            }
-
-            spawnWaveDelay = currentWave.waveDelay;
-            if (spawnRoutine != null) StopCoroutine(spawnRoutine);
-            spawnRoutine = StartCoroutine(SpawnEnemies());
-        }
-    }
-
-    private IEnumerator SpawnEnemies()
-    {
-        yield return new WaitForSeconds(spawnWaveDelay);
-
-        foreach (var enemyData in currentWave.enemies)
-        {
-
-            yield return new WaitForSeconds(enemyData.spawnDelay);
-
-            for (int i = 0; i < enemyData.amount; i++)
-            {
-                ActivateEnemy(enemyData.enemyType);
-                yield return new WaitForSeconds(enemyData.spawnRate);
-            }
-        }
-    }
-
-    private void ActivateEnemy(GameObject enemyType)
-    {
-        if (enemyPool.ContainsKey(enemyType) && enemyPool[enemyType].Count > 0)
-        {
-            GameObject enemy = enemyPool[enemyType].Dequeue();
-            EnemyProperties enemyProps = enemy.GetComponent<EnemyProperties>();
-            enemyProps.SetEnemyManager(this);
-            enemyProps.PrefabReference = enemyType;
-            enemyProps.Respawn((float)GameManager.Instance.GetCurrentWave()/10);
-            activeObjects.Add(enemy);
-        }
-    }
-
-    public void DeactivateEnemy(GameObject enemy)
-    {
-        activeObjects.Remove(enemy);
-
-        EnemyProperties enemyProps = enemy.GetComponent<EnemyProperties>();
-        if (enemyProps != null && enemyProps.PrefabReference != null && enemyPool.ContainsKey(enemyProps.PrefabReference))
-        {
-            enemyPool[enemyProps.PrefabReference].Enqueue(enemy);
-        }
-        else
-        {
-            Debug.LogError("Prefab reference not found in enemyPool for: " + enemy.name);
-        }
-        if (activeObjects.Count == 0) currentWave = null;
     }
 
     public void StopMovementForSeconds(float seconds)
@@ -118,35 +47,21 @@ public class EnemyMovement : MonoBehaviour
 
     private IEnumerator StopMovementCoroutine(float seconds)
     {
-        foreach (GameObject enemy in activeObjects)
-        {
-            EnemyProperties enemyProps = enemy.GetComponent<EnemyProperties>();
-            if (enemyProps != null)
-            {
-                enemyProps.isStopped = true;
-            }
-        }
+        isStopped = true; 
         yield return new WaitForSeconds(seconds); 
-        foreach (GameObject enemy in activeObjects)
-        {
-            EnemyProperties enemyProps = enemy.GetComponent<EnemyProperties>();
-            if (enemyProps != null)
-            {
-                enemyProps.isStopped = false;
-            }
-        }
+        isStopped = false; 
     }
 
-    // public void SlowDownForSeconds(float slowSpeed, float duration)
-    // {
-    //     StartCoroutine(SlowDownCoroutine(slowSpeed, duration));
-    // }
+    public void SlowDownForSeconds(float slowSpeed, float duration)
+    {
+        StartCoroutine(SlowDownCoroutine(slowSpeed, duration));
+    }
 
-    // private IEnumerator SlowDownCoroutine(float slowSpeed, float duration)
-    // {
-    //     float originalSpeed = speed; 
-    //     speed = slowSpeed; 
-    //     yield return new WaitForSeconds(duration); 
-    //     speed = originalSpeed; 
-    // }
+    private IEnumerator SlowDownCoroutine(float slowSpeed, float duration)
+    {
+        float originalSpeed = speed; 
+        speed = slowSpeed; 
+        yield return new WaitForSeconds(duration); 
+        speed = originalSpeed; 
+    }
 }
